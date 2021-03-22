7eb065ee1c4c69af2b333b34e60e5436eda8b675
camel25 <79445726+camel25@users.noreply.github.com>
2021-03-22 13:34:10 +0200

    Storage exercise

--- js/20-pcStorage.js
+++ js/20-pcStorage.js
@@ -4,36 +4,86 @@ Duota kietojo disko duomenu struktura (folder > file) ir reikia suskaiciuoti, ki
 Folder uzima 0 vietos
 */
 
-function storage(params) {
-    return 0 ;
+function storage(folder) {
+    let totalSize = 0 ;
+    for (let i = 0 ; i < folder.content.length; i++) {
+        const child = folder.content[i];
+
+        if (child.type === 'file') {
+            totalSize += child.size
+        } else if ( child.type === 'folder') {
+            totalSize += storage(child)
+        }
+    }
+
+    return totalSize ;
 }
 
 const pc = {
-    name: 'c',
+    name: 'C',
     type: 'folder',
     content: [
         {
-            name: 'Program Files',
-            type: 'folder',
-            content: []
-        },
-        {
-            name: 'Winodws',
+            name: 'Programs Files',
             type: 'folder',
             content: [
                 {
-                    
+                    name: 'Android',
+                    type: 'folder',
+                    content: []
+                },
+                {
+                    name: 'GIMP 2',
+                    type: 'folder',
+                    content: [
+                        {
+                            name: 'dbghelp.dll',
+                            type: 'file',
+                            size: 1497,
+                            sizeUnits: 'kb'
+                        },
+                        {
+                            name: 'gimp-2.10.exe',
+                            type: 'file',
+                            size: 9138,
+                            sizeUnits: 'kb'
+                        }
+                    ]
+                },
+            ]
+        },
+        {
+            name: 'Windows',
+            type: 'folder',
+            content: [
+                {
+                    name: 'DiagTrack',
+                    type: 'folder',
+                    content: [
+                        {
+                            name: 'analyticsevents.dat',
+                            type: 'file',
+                            size: 2,
+                            sizeUnits: 'kb'
+                        },
+                        {
+                            name: 'utc.allow.diffbase',
+                            type: 'file',
+                            size: 468,
+                            sizeUnits: 'kb'
+                        }
+                    ]
                 }
             ]
         },
         {
             name: 'msdia80.dll',
-            type: 'folder',
-            size:884,
+            type: 'file',
+            size: 884,
             sizeUnits: 'kb'
         }
     ]
 }
 
-const totalKbs = storage(pc);
-console.log('Used space:', totalKbs, 'Kbs');
+const totalKBs = storage(pc);
+console.log('Used space:', totalKBs, 'KB');
